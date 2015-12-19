877b53d5961eade7281171edc4b843776903fba9
Burak <burakiren@hotmail.com.tr>
2015-12-19 21:45:01 +0200

    Initial commit

---
+++ app/src/main/java/com/burakiren/contactmanager/MainActivity.java
@@ -0,0 +1,146 @@
+package com.burakiren.contactmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TabHost;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nameTxt, phoneTxt, emailTxt, adresTxt;
+    ImageView contactImgView;
+    //List<Contact> Contacts = new ArrayList<Contact>();
+    ListView contactListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTxt = (EditText) findViewById(R.id.txtName);
+        phoneTxt = (EditText) findViewById(R.id.txtPhone);
+        emailTxt = (EditText) findViewById(R.id.txtEmail);
+        adresTxt = (EditText) findViewById(R.id.txtAdres);
+        //contactListView = (ListView) findViewById(R.id.listView);
+        contactImgView = (ImageView) findViewById(R.id.imgViewContactImage);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("creator");
+        tabSpec.setContent(R.id.creatorTab);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("list");
+        tabSpec.setContent(R.id.contactTab);
+        tabSpec.setIndicator("List");
+        tabHost.addTab(tabSpec);
+
+        final Button addBtn = (Button) findViewById(R.id.btnEkle);
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                addBtn.setEnabled(!nameTxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        contactImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Contact Image"), 1);
+
+            }
+        });
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int reqCode, int resCode, Intent data)
+    {
+        if(resCode == RESULT_OK)
+        {
+            contactImgView.setImageURI(data.getData());
+        }
+    }
+    private void populateList()
+    {
+        //ArrayAdapter<Contact> adapter = new ContactListAdapter();
+        //cotactListView.setAdapter(adapter);
+    }
+
+    private void addContact(String name, String phone, String email, String address)
+    {
+        //Contacts.add(new Contact(name, phone, email, address));
+    }
+
+    private class ContactListAdapter extends ArrayAdapter<Contact>
+    {
+        public ContactListAdapter()
+        {
+            super (MainActivity.this, R.layout.listview_item, Contacts);
+        }
+    }
+}
